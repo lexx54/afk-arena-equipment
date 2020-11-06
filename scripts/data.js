db1de8e552ff7bf096ecf87b02b6fc55bec362e8
lexx54 <chapelexx54@gmail.com>
2020-11-06 16:03:37 -0400

    cleaning function added, to reset all the values of the form

--- scripts/data.js
+++ scripts/data.js
@@ -3,8 +3,8 @@ export function setData(...values){
       $equipment=document.getElementById(values[1]).children,
       $star=document.getElementById(values[2]),
       $faction=document.getElementById(values[3]).children,
-      $level=document.getElementById(values[4]).children,
-      data={};
+      $level=document.getElementById(values[4]).children;
+      
 
   let $heroNameValue=$heroName.value,
     $equipmentValue=equipmentChecker($equipment),
@@ -12,23 +12,26 @@ export function setData(...values){
     $factionValue=factionChecker($faction),
     $levelValue=levelChecker($level);
 
+  const data=new Hero($heroNameValue,$equipmentValue,$starValue,$factionValue,$levelValue);
+  cleaning(values);
+  return data
   
 
   // console.log($equipmentValue)
-    if ($heroNameValue==="") $heroName.focus();
-    else if ($starValue==="") $star.focus();
-    else if ($factionValue==="")$faction.focus();
-    else if ($levelValue==="")$level.focus();
-    else{
-      // localStorage.setItem("nombre",$nameValue);
-      // localStorage.setItem("age",$ageValue)
-      data.push($heroNameValue,$starValue,$factionValue,$levelValue);
-      document.getElementById(values[0]).value="";
-      document.getElementById(values[1]).value="";
-      document.getElementById(values[2]).value="";
-      document.getElementById(values[3]).value="";
-      return data;
-    } 
+    // if ($heroNameValue==="") $heroName.focus();
+    // else if ($starValue==="") $star.focus();
+    // else if ($factionValue==="")$faction.focus();
+    // else if ($levelValue==="")$level.focus();
+    // else{
+       // localStorage.setItem("nombre",$nameValue);
+       // localStorage.setItem("age",$ageValue)
+       // data.push($heroNameValue,$starValue,$factionValue,$levelValue);
+    //   document.getElementById(values[0]).value="";
+    //   document.getElementById(values[1]).value="";
+    //   document.getElementById(values[2]).value="";
+    //   document.getElementById(values[3]).value="";
+    //   return data;
+    // } 
   
 }
 
@@ -57,7 +60,24 @@ function factionChecker(faction){
 }
 
 function levelChecker(level){
-  for (let ele of faction){
+  for (let ele of level){
     if (ele.checked===true) return ele.value;
   }
+}
+function cleaning(list){
+  for (let i=0 ; i<list.length ; i++){
+    if (i===0 ||i===2){
+      document.getElementById(list[i]).value="";
+    } else {
+      let listC=document.getElementById(list[i]).children
+      for (let ele of listC){
+        if (ele.type==="radio") ele.checked=false;
+      }
+    }
+  }
+}
+
+function Hero(name,equipment,star,faction,level){
+  this.name=name;
+  this[equipment]={star,faction,level}
 }
